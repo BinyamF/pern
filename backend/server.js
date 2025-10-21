6944f5b6dbc478d66e6f51b305076fe60bdfe962
Binyam Fitamo <binyamfitamo7@gmail.com>
2025-10-21 15:26:24 +0300

    made corrections

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const port = 3000;
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());    //security middleware that [rotect the app by setting diif values to headers
+app.use(morgan("dev"));    //logs requests on the console
 
 app.get("/",(req,res) => {
   res.send('Express end point');
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`) ;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`) ;
 })
